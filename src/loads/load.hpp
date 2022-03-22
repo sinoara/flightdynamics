6e368971eb54214e98f46c5ab29a023750a68c93
Guilherme Sinoara <guilherme@elemental-future.com>
2022-03-22 22:57:19 +0100

    added comments

--- src/loads/load.hpp
+++ src/loads/load.hpp
@@ -6,11 +6,14 @@
 
 class Load {
 	public:
+		// forces caused by Load in earth axis
 		virtual std::array<float, 3> forces() = 0;
+		// moments caused by Load in earth axis
 		virtual std::array<float, 3> moments() = 0;
 
 		Load(System *parent);
 
 	private:
+		// parent System of the Load
 		System *parent_;
 };
