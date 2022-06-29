c488ed0241d9266f110a7fa386ce17bec0142e0d
Guilherme Sinoara <gsinoara@gmail.com>
2022-06-29 13:09:02 -0300

    added comments to loads/weight.cpp

--- src/loads/weight.cpp
+++ src/loads/weight.cpp
@@ -2,9 +2,12 @@
 #include "../constants.hpp"
 
 std::array<float, 3> Weight::forces() {
+	// force in earth axis and on the CG
 	return std::array<float, 3> {0.0, 0.0, 0.0};
 };
 
 std::array<float, 3> Weight::moments() {
+	// moment in earth axis and on the CG
+	// always zero
 	return std::array<float, 3> {0.0, 0.0, 0.0};
 };
