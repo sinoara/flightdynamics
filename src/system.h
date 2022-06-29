41a33f9e684edfcaeceb07fcdfcabd0ae1903205
Guilherme Sinoara <gsinoara@gmail.com>
2022-06-29 13:09:01 -0300

    added includes and methods for inertia

--- src/system.h
+++ src/system.h
@@ -2,17 +2,23 @@
 #define SYSTEM_H
 
 #include <array>
+#include <vector>
+#include <memory>
 
 
 class System {
   public:
-    float moment_inertia[3][3];
+    float momentOfInertia[3][3];
     float mass;
     float position[3];
     float velocity[3];
+	std::vector<std::shared_ptr<System>> systems;
 
     std::array<float, 3> forces();
     std::array<float, 3> acceleration();
+	float totalMass();
+	float totalMomentOfInertia();
+	void addSystem(System sys);
 
     System(float mass);
 };
