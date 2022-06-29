3c4df134dd2c4e5ca13815c5a8883727b16f4e09
Guilherme Sinoara <gsinoara@gmail.com>
2022-06-29 13:09:10 -0300

    changed parent_ member attribute to be private

--- src/loads/load.hpp
+++ src/loads/load.hpp
@@ -6,10 +6,11 @@
 
 class Load {
 	public:
-		System *parent_;
-
 		virtual std::array<float, 3> forces() = 0;
 		virtual std::array<float, 3> moments() = 0;
 
 		Load(System *parent);
+
+	private:
+		System *parent_;
 };
