22617b8984bb15e8f8779e2e1e33a112ee389a9f
Guilherme Sinoara <gsinoara@gmail.com>
2022-06-29 13:09:01 -0300

    added file with constants

--- src/vehicle.h
+++ src/vehicle.h
@@ -8,16 +8,6 @@
 
 class Vehicle: public System {
   public:
-    /*
-    float moment_inertia[3][3];	  
-    float mass;
-    float position[3];
-    float velocity[3];
-
-    std::array<float, 3> forces();
-    std::array<float, 3> acceleration();
-
-	*/
     Vehicle(float mass) : System(mass) {}
 };
 
