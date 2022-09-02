c3a8e66e0045f7f30c3edffda477d267ec99d198
Guilherme Sinoara <gsinoara@gmail.com>
2022-09-02 16:33:01 -0300

    fixed indentation

--- src/system/system.hpp
+++ src/system/system.hpp
@@ -11,13 +11,14 @@ class System {
     float mass;
     float position[3];
     float velocity[3];
-	std::vector<std::shared_ptr<System>> systems;
+    std::vector<std::shared_ptr<System>> systems;
 
     std::array<float, 3> forces();
     std::array<float, 3> acceleration();
-	float totalMass();
-	float totalMomentOfInertia();
-	void addSystem(System sys);
+
+    float totalMass();
+    float totalMomentOfInertia();
+    void addSystem(System sys);
 
     System(float mass);
 };
--- tests/test_loads.cpp
+++ tests/test_loads.cpp
@@ -8,36 +8,36 @@
 
 
 TEST(LoadsTest, WeightForcesValueTest){
-	// map with system masses and expected values to be tested
-	std::map<float, std::array<float, 3>> testValues {
-	  	{0.0, std::array<float, 3> {0.0, 0.0, 0.0}},
-	  	{1.0, std::array<float, 3> {0.0, 0.0, constants::g*1.0f}},
-	  	{3.5, std::array<float, 3> {0.0, 0.0, constants::g*3.5f}},
-	};
+    // map with system masses and expected values to be tested
+    std::map<float, std::array<float, 3>> testValues {
+        {0.0, std::array<float, 3> {0.0, 0.0, 0.0}},
+            {1.0, std::array<float, 3> {0.0, 0.0, constants::g*1.0f}},
+            {3.5, std::array<float, 3> {0.0, 0.0, constants::g*3.5f}},
+    };
 
-	// loop through map and test values
-	for (auto const& [input, expected] : testValues) {
-		System system(input);
-		Weight weight(&system);
-		
-		ASSERT_EQ(expected, weight.forces());
-	}
+    // loop through map and test values
+    for (auto const& [input, expected] : testValues) {
+        System system(input);
+        Weight weight(&system);
+
+        ASSERT_EQ(expected, weight.forces());
+    }
 }
 
 TEST(LoadsTest, WeigthMomentsTest){
-	// map with system masses and expected values to be tested
-	std::array<float, 3> zeroArray =  {0.0, 0.0, 0.0};
-	std::map<float, std::array<float, 3>> testValues {
-	  	{0.0, zeroArray},
-	  	{1.0, zeroArray},
-	  	{3.5, zeroArray},
-	};
+    // map with system masses and expected values to be tested
+    std::array<float, 3> zeroArray =  {0.0, 0.0, 0.0};
+    std::map<float, std::array<float, 3>> testValues {
+        {0.0, zeroArray},
+            {1.0, zeroArray},
+            {3.5, zeroArray},
+    };
 
-	// loop through map and test values
-	for (auto const& [input, expected] : testValues) {
-		System system(input);
-		Weight weight(&system);
+    // loop through map and test values
+    for (auto const& [input, expected] : testValues) {
+        System system(input);
+        Weight weight(&system);
 
-		ASSERT_EQ(expected, weight.moments());
-	}
+        ASSERT_EQ(expected, weight.moments());
+    }
 }
